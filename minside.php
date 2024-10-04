5d3b474a950d6057648a499c3c47e47dbb372865
emmao <emf002@edu.zealand.dk>
2024-10-04 12:34:35 +0200

    Sat overskrift og tilbage knap i samme container for sig selv

--- minside.php
+++ minside.php
@@ -35,27 +35,29 @@ $newEventsCount = $newEvents[0]->newEventsCount;
 
 <!-- Baggrundsbillede med overlay -->
 <div style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; overflow: hidden; z-index: -1;">
-    <img src="images/baggrundsbillede.jpg" alt="background" class="position-absolute top-0 start-0 w-100 h-100 object-fit-cover" style="filter: blur(0px);">
+    <img src="images/baggrundsbillede.jpg" alt="background"
+         class="position-absolute top-0 start-0 w-100 h-100 object-fit-cover" style="filter: blur(0px);">
 
     <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background: rgba(0, 0, 0, 0.4); z-index: 1;"></div>
 </div>
 
 
 <div class="container">
-
     <div class="row">
         <div class="py-5">
             <p class="overskrift-stor text-white">Min side</p>
             <a href="index.php" class="text-decoration-underline tilbageknap brødtekst-knap">Tilbage</a>
         </div>
     </div>
+</div>
 
-
+<div class="container">
     <div class="row w-100 mt-5">
         <div class="col-8 col-sm-10 col-md-8 col-lg-6 col-xl-4 offset-2 offset-sm-1 offset-md-2 offset-lg-3 offset-xl-4 ">
             <div class="mb-4">
                 <a href="eventsoprettetafmig.php">
-                    <button class="btn btn-minside w-100 rounded-pill p-3 brødtekst-knap">Events oprettet af mig</button>
+                    <button class="btn btn-minside w-100 rounded-pill p-3 brødtekst-knap">Events oprettet af mig
+                    </button>
                 </a>
             </div>
 
