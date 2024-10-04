fa45a253e267ebed1f6fc56ec260e763b630d000
emmao <emf002@edu.zealand.dk>
2024-10-04 11:13:35 +0200

    Opsat koden pænt

--- index.php
+++ index.php
@@ -15,17 +15,17 @@ require "settings/init.php";
     <link href="css/styles.css" rel="stylesheet" type="text/css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
 </head>
-
-
-
 <body>
+
+<!-- Baggrundsbillede -->
 <div style="position: fixed; top: 0; left: 0; width: 100%; height: 100%; overflow: hidden; z-index: -1;">
-    <img src="images/background1.webp" alt="background" class="position-absolute top-0 start-0 w-100 h-100"
-         style="object-fit: cover;">
+    <img src="images/background1.webp" alt="background" class="position-absolute top-0 start-0 w-100 h-100 object-fit-cover">
 </div>
 
-<!-- Første container til overskrift og tekst -->
+
+<!-- Overskrift og tekst -->
 <div class="container d-flex flex-column justify-content-center align-items-center vh-100">
     <div class="row">
         <div class="col-12 text-center text-light fs-1">
@@ -36,7 +36,7 @@ require "settings/init.php";
         </div>
     </div>
 
-
+<!-- Log på -->
     <div class="row w-100">
         <div class="col-8 col-sm-10 col-md-8 col-lg-6 col-xl-4 offset-2 offset-sm-1 offset-md-2 offset-lg-3 offset-xl-4">
             <form action="minside.php" method="post">
@@ -57,14 +57,14 @@ require "settings/init.php";
         </div>
     </div>
 
-
 </div>
-
 
 
 <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 
 <script>
+
+    // Typewriter tekst
 
     const featureText = document.querySelector("#featureText");
     const carouselList = [
@@ -114,6 +114,7 @@ require "settings/init.php";
     function waitFor(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
     }
+
 
 </script>
 </body>
